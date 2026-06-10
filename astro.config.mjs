d9dfc23b288dfd6e35219c26bd39772aa6b1509c
Junichiro Imai <jnicrimi@gmail.com>
2026-06-11 00:52:43 +0900

    feat: add ogp meta tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://jnicrimi.com",
   integrations: [react(), icon()],
   vite: {
     plugins: [tailwindcss()],
